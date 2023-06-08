738a1222c2e0ab62b0906affc79569b48d7dc8ff
Naim <naim.nanocoding@gmail.com>
2023-06-08 08:40:10 +0500

    fix

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -1,17 +1,9 @@
-import { validationResult } from 'express-validator'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import jsonwebtoken from 'jsonwebtoken'
 
 export const __filename = fileURLToPath(import.meta.url)
 export const __dirname = dirname(__filename)
-
-export const validationError = (res, req, next) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
-  }
-}
 
 export const buildUser = (user) => {
   const userClone = JSON.parse(JSON.stringify((user)))
